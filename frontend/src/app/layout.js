373e73d746555aa0627af15c38ddd499e26f7b1c
Lazar Petrovic <lazar.petrovic.nefti@gmail.com>
2024-01-04 14:00:59 +0100

    Fix navbar expansion.
    Problem was that we needed the nexuiprovider around the navbar component inside our page.js file and not in the layout.js file.

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -3,14 +3,13 @@
 import React from "react";
 import {Inter} from 'next/font/google'
 import './globals.css'
-import {NextUIProvider} from "@nextui-org/react";
 
 const inter = Inter({subsets: ['latin']})
 
 export default function RootLayout({children}) {
     return (
-        <NextUIProvider className="dark text-foreground bg-background">
+        <html lang="en" className="dark text-foreground bg-background">
             <body className={inter.className}>{children}</body>
-        </NextUIProvider>
+        </html>
     )
 }
--- frontend/src/components/navbar_component/navbar.js
+++ frontend/src/components/navbar_component/navbar.js
@@ -1,17 +1,56 @@
 'use client'
 
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    Link,
+    Button,
+    NavbarMenuToggle,
+    NavbarMenu, NavbarMenuItem
+} from "@nextui-org/react";
 import {Logo} from "@/assets/logo";
 
 export default function NavbarComponent() {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const menuItems = [
+        "Profile",
+        "Dashboard",
+        "Activity",
+        "Analytics",
+        "System",
+        "Deployments",
+        "My Settings",
+        "Team Settings",
+        "Help & Feedback",
+        "Log Out",
+    ];
+
     return (
-        <Navbar shouldHideOnScroll>
-            <NavbarBrand>
-                <Logo/>
-                <p className="font-bold text-inherit">ACME</p>
-            </NavbarBrand>
+        <Navbar
+            isBordered
+            isMenuOpen={isMenuOpen}
+            onMenuOpenChange={setIsMenuOpen}
+        >
+            <NavbarContent className="sm:hidden" justify="start">
+                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"}/>
+            </NavbarContent>
+
+            <NavbarContent className="sm:hidden pr-3" justify="center">
+                <NavbarBrand>
+                    <Logo/>
+                    <p className="font-bold text-inherit">PREMIUMGLANZ</p>
+                </NavbarBrand>
+            </NavbarContent>
+
             <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarBrand>
+                    <Logo/>
+                    <p className="font-bold text-inherit">PREMIUMGLANZ</p>
+                </NavbarBrand>
                 <NavbarItem>
                     <Link color="foreground" href="#">
                         Features
@@ -28,17 +67,34 @@ export default function NavbarComponent() {
                     </Link>
                 </NavbarItem>
             </NavbarContent>
+
             <NavbarContent justify="end">
                 <NavbarItem className="hidden lg:flex">
                     <Link href="#">Login</Link>
                 </NavbarItem>
                 <NavbarItem>
-                    <Button as={Link} color="primary" href="#" variant="flat">
+                    <Button as={Link} color="warning" href="#" variant="flat">
                         Sign Up
                     </Button>
                 </NavbarItem>
             </NavbarContent>
-        </Navbar>
 
+            <NavbarMenu>
+                {menuItems.map((item, index) => (
+                    <NavbarMenuItem key={`${item}-${index}`}>
+                        <Link
+                            className="w-full"
+                            color={
+                                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
+                            }
+                            href="#"
+                            size="lg"
+                        >
+                            {item}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+        </Navbar>
     )
 }
