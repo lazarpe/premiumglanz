85b52eae49e3b51dfdae36536c5e70bf97a0eb73
Lazar Petrovic <lazar.petrovic.nefti@gmail.com>
2024-01-04 08:09:03 +0100

    Setup nextjs app and create navbar.

---
+++ frontend/src/app/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import * as React from "react";
+
+import NavbarComponent from "../components/navbar_component/navbar";
+import {NextUIProvider} from "@nextui-org/react";
+
+export default function Home() {
+    return (
+        <NextUIProvider>
+            <NavbarComponent/>
+        </NextUIProvider>
+    )
+}
